0f50fcdcf2c07e7998dfb8d3481d9e59d2d294a9
Valentin Lafage <valentin.lafage@celadodc-rswl.com>
2025-04-24 22:46:42 +0200

    feat: set up di
    Signed-off-by: Valentin Lafage <valentin.lafage@celadodc-rswl.com>

--- src/resumex/__main__.py
+++ src/resumex/__main__.py
@@ -3,18 +3,24 @@ import logging
 import logging.config
 
 from resumex.config import Paths
+from resumex.core.di import ServiceProvider
 
 
 def main():
     set_up_logging()
+    init_dependency_providers()
 
 
 def set_up_logging():
     os.makedirs(Paths.LOGS, exist_ok=True)
     logging.config.fileConfig(
         Paths.CONFIG.joinpath("logging.conf"),
-        defaults={"fname": str(Paths.LOGS.joinpath("resumex.log"))},
+        defaults={"fname": Paths.LOGS.joinpath("resumex.log")},
     )
+
+
+def init_dependency_providers():
+    ServiceProvider()
 
 
 if __name__ == "__main__":
